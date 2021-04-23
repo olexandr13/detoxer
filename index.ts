97cfb7b740896e872fb5bc23b7b3ec47ab8c8212
olexandr13 <oleksandr.pelykh@gmail.com>
2021-04-23 18:18:33 +0300

    add index file

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { $, $$ } from './src/element';
+import { helpers } from './src/helpers';
+import { log } from './src/logger';
+
+export default {
+  $: $,
+  $$: $$,
+  helpers: helpers,
+  log: log,
+}
