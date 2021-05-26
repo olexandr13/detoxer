9fd970d2b6ea2a3a9f343aa8e0e205f71b4e61ba
olexandr13 <oleksandr.pelykh@gmail.com>
2021-05-26 15:51:16 +0300

    upd

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,16 @@
-export { app } from './app';
-export { assert } from './assertions';
-export { $, $$ } from './element';
-export { helpers } from './helpers';
-export { log } from './logger';
+export declare const app1: {
+    clearApp(): Promise<void>;
+    launchAppWithPermissions(): Promise<void>;
+};
+export declare const assert1: {
+    should: {
+        be: {
+            truthy: (anything: any, errorMessage?: string | undefined) => void;
+        };
+        equal: (value1: any, value2: any) => void;
+    };
+};
+export declare const helpers1: {
+    sleep(seconds: number): Promise<void>;
+    prettyStringify(json: any): string;
+};
