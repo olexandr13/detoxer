045146f49b1ec6449f4f884bb23e7cb7fb21209e
Oleksandr Pelykh <olexandr13@gmail.com>
2022-02-22 12:18:56 +0200

    add detox and device to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+export { device } from 'detox';
 export { app } from './app';
 export { assert } from './assertions';
 export { $, $$ } from './element';
 export { helpers } from './helpers';
 export { log } from './logger';
+export { detox };
+import * as detox from 'detox';
