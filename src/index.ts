afd4c46236993aa3e55d627bd86e6f5f567799b6
Oleksandr Pelykh <olexandr13@gmail.com>
2022-02-22 12:13:31 +0200

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { app } from './app';
 export { assert } from './assertions';
-export { $, $$, SwipeDirection } from './element';
+export { $, $$ } from './element';
 export { helpers } from './helpers';
 export { log } from './logger';
--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 import { Logger, TLogLevelName } from 'tslog';
 
-const logLevel: TLogLevelName = process.env.LOG_LEVEL as TLogLevelName || 'info';
+const logLevel: TLogLevelName = process.env.LOG_LEVEL as TLogLevelName || 'debug';
 
 export const log: Logger = new Logger({ minLevel: logLevel, colorizePrettyLogs: true });
